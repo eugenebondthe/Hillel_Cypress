b99ea8cbf290f8f329f8e0ab6903f5eb87c31d78
Eugene Bondarenko <eugenebondthe@gmail.com>
2024-04-16 13:20:28 +0200

    Resolving concerns

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,25 +1,29 @@
 describe("New user registration flow", () => {
+  const name = "Vasiliy";
+  const lastName = "Pupkin";
+  const password = "Password123";
+  let randomEmail;
+
+  beforeEach(() => {
+    randomEmail = generateRandomEmail();
+  })
+
   it("Successful registration", () => {
     cy.visit("https://guest:welcome2qauto@qauto2.forstudy.space/");
     cy.get(".hero-descriptor_btn").contains("Sign up").click();
 
-    const randomEmail = generateRandomEmail();
-    const name = "Vasiliy";
-    const lastName = "Pupkin";
-    const password = "Password123";
-
-    cy.get('input[name="name"]').type(name);
-    cy.get('input[name="lastName"]').type(lastName);
-    cy.get('input[name="email"]').type(randomEmail);
-    cy.get('input[name="password"').type(password);
-    cy.get('input[name="repeatPassword"]').type(password);
+    cy.get('input[name="name"]').type(name).should('have.value', name);
+    cy.get('input[name="lastName"]').type(lastName).should('have.value', lastName);
+    cy.get('input[name="email"]').type(randomEmail).should('have.value', randomEmail);
+    cy.get('input[name="password"').type(password).should('have.value', password);
+    cy.get('input[name="repeatPassword"]').type(password).should('have.value', password);
 
     cy.contains("button", "Register").click();
     cy.url().should("include", "qauto2.forstudy.space/panel/garage");
 
     cy.get("#userNavDropdown").click();
     cy.get('[routerlink="/panel/profile"]').click();
-    cy.contains(name || lastName);
+    cy.get('.profile_name').should('contain.text', 'Vasiliy');
   });
 });
 
